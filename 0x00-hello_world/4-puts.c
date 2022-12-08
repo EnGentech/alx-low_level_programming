2ca63f04c6d4320cd97419355f181f1163ac7969
EnGentech <engen.inyang@gmail.com>
2022-12-08 05:31:17 -0800

    re-edited to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(){
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
