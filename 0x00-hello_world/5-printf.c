6e294d61d78dfb52de3937fec3c8f8758ea5c328
EnGentech <engen.inyang@gmail.com>
2022-12-08 05:47:53 -0800

    Print function completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *this program will print using the printf statement 
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
