5d076c63b20c0770790263ba8dcfdbda6ed956ee
EnGentech <engen.inyang@gmail.com>
2022-12-09 10:01:19 -0800

    added space as intructed by betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 char alphabet;
-for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 putchar('\n');
 return (0);
