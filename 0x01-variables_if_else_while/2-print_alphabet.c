95d4f7b7e5e5f5dcbc6e7940600ad0b60ffce986
EnGentech <engen.inyang@gmail.com>
2022-12-09 09:58:40 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *This program will print all lower case of the alphabet
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet;
+for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
