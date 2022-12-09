7944cf5107157a6ef459b1f70b65e8142de56f1a
EnGentech <engen.inyang@gmail.com>
2022-12-09 15:16:06 -0800

    combinning values in two pairs

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for combining two is to two input
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int j, i;
+for (j = 0; j <= 99; j++)
+{
+for (i = j; i <= 99; i++)
+{
+if (i != j)
+{
+putchar(j / 10 + 48);
+putchar(j % 10 + 48);
+putchar(' ');
+putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+if (j * 100 + i != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
