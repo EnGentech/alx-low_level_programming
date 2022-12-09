796f9e3177873c37bc4a2b75aea943330c2cda82
EnGentech <engen.inyang@gmail.com>
2022-12-09 10:48:42 -0800

    Numbers in unit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print only values in units
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num <=9; num++)
+printf("%d", num);
+putchar('\n');
+return (0);
+}
