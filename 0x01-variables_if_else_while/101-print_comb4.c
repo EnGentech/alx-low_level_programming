5befc8b2695c120af45453c43d26dd78b8381f9d
EnGentech <engen.inyang@gmail.com>
2022-12-09 14:50:42 -0800

    three possible combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-  int n, i, z;
-  for (n = 0; n <= 7; n++)
-    {
-      for (i = n + 1; i <= 8; i++)
-	{
-	  for (z = i + 1; z <= 9; z++)
-	    {
-	      putchar((n % 10) + '0');
-	      putchar((i % 10) + '0');
-	      putchar((z % 10) + '0');
-	      if (n == i || n == z)
-		continue;
-	      else if (n == 7 && i == 8 && z == 9)
-		{
-	      break;
-		}
-	      else
-		{
-	      putchar(',');
-	      putchar(' ');
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int n, i, z;
+for (n = 0; n <= 7; n++)
+{
+for (i = n + 1; i <= 8; i++)
+{
+for (z = i + 1; z <= 9; z++)
+{
+putchar((n % 10) + '0');
+putchar((i % 10) + '0');
+putchar((z % 10) + '0');
+if (n == i || n == z)
+continue;
+else if (n == 7 && i == 8 && z == 9)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
