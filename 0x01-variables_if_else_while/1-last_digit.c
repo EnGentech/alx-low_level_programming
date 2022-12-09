67350ae80def9413a400f3b45143e2227386ae8f
EnGentech <engen.inyang@gmail.com>
2022-12-09 09:40:45 -0800

    removed trailling white space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,6 @@ int main(void)
 {
 int n;
 int l_dgt;
-  
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 l_dgt = n % 10;
@@ -30,6 +29,5 @@ else if (l_dgt < 6 && l_dgt != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_dgt);
 }
-  
 return (0);
 }
