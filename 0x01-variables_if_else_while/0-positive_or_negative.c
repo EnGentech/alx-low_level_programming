5cb06f20cb1d112fb935ceec199ce6461341fd10
EnGentech <engen.inyang@gmail.com>
2022-12-09 08:56:36 -0800

    re-edited conditional

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
-  printf("zero\n");
+printf("%d is zero\n", n);
 }
 else if (n > 0)
 {
-  printf("is positive\n");
+printf("%d is positive\n", n);
 }
 else
 {
-  printf("is negative\n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
