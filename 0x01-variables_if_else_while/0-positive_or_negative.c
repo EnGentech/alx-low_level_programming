2eeba9adf84c3bf2465cb011d61a06f447797805
EnGentech <engen.inyang@gmail.com>
2022-12-09 08:01:56 -0800

    re-edited with standard i/o on conditionals

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  *main - Entry point
  *
@@ -15,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
-printf("zero\n");
+  printf("zero\n");
 }
 else if (n > 0)
 {
-printf("is positive\n");
+  printf("is positive\n");
 }
 else
 {
-printf("is negative\n");
+  printf("is negative\n");
 }
 return (0);
 }
