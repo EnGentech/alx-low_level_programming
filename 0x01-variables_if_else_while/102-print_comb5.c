18ba342e5681a7fc69b09f9c44d62af93fa0efc0
EnGentech <engen.inyang@gmail.com>
2022-12-09 15:21:03 -0800

    modified betty style on two two combinational digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point for combining two is to two input
- * 
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 
 int main(void)
