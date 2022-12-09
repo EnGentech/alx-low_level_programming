18b42af87a0368b3cfe2f886a3b2416854e454c0
EnGentech <engen.inyang@gmail.com>
2022-12-09 11:38:26 -0800

    values generation

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
+      
