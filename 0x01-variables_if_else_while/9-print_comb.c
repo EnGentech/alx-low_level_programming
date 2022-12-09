eb4ea3e366d9ac9aa419cccbbd3dd34c48f67a9a
EnGentech <engen.inyang@gmail.com>
2022-12-09 13:00:18 -0800

    iterating numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Hex values
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
