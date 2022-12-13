aa94bfb650b40e62c34afe237c10243dc120805a
EnGentech <engen.inyang@gmail.com>
2022-12-13 08:51:36 -0800

    10 x alphabet prototype

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int count;
+char lalpha;
+for (count = 1; count <= 10; count++)
+{
+for (lalpha = 'a'; lalpha <= 'z'; lalpha++)
+{
+_putchar(lalpha);
+}
+_putchar('\n');
+}
