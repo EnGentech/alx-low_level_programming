a97866af6fcb5b9522d6410d24cb6a1fc912bbe8
EnGentech <engen.inyang@gmail.com>
2022-12-13 11:46:08 -0800

    print only absolute values and convert negative to positive

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @x: return positive values and ignore the negative sign
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+if (x >= 0)
+return (x);
+else
+return (-x);
+}
