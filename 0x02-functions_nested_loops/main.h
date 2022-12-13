a0a16a321c41406e70b06924c75b9730c3eee8b0
EnGentech <engen.inyang@gmail.com>
2022-12-13 10:33:44 -0800

    modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _islower(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,3 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+#endif
