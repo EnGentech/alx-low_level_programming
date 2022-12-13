777ee549afa5f80d773bf35f9ad72ec6f5aa290c
EnGentech <engen.inyang@gmail.com>
2022-12-13 04:49:46 -0800

    added main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+char put[8] = "_putchar";
+int i;
+for(i = 0; put[i]; i++)
+  {
+    putchar(put[i]);
+  }
+putchar('\n');
+return (0);char put[8] = "_putchar";
+int i;
+for(i = 0; put[i]; i++)
+  {
+    putchar(put[i]);
+  }
+putchar('\n');
+return (0);
