781fc80ed5ef1808507830853a7e3840006645bc
EnGentech <engen.inyang@gmail.com>
2022-12-13 09:44:08 -0800

    10 times a-z prototype

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * 10 times a-z prototype - entry point
+ * print_alphabet_x10 - entry point
  *
  * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
-  int i;
-  char j;
-  i = 1;
-  while i <= 10;
-  {
-    for (j = 'a'; j <= 'z'; j++)
-      {
-	_putchar(j);
-      }
-    _putchar(i);
-  }
+int i;
+char j;
+i = 1;
+
+while (i <= 10);
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar(i);
+}
 }
