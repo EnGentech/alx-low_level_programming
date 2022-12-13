21607ebab61ab812f6298801aa0f195bc4121a80
EnGentech <engen.inyang@gmail.com>
2022-12-13 08:35:11 -0800

    modified content

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,8 @@
 
 void print_alphabet(void)
 {
- char alphabet;
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-          putchar(alphabet);
-      putchar('\n');
- }
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+}
