6449df77771fb1e712bc77529a64457036d38f49
EnGentech <engen.inyang@gmail.com>
2022-12-13 11:31:16 -0800

    return values and signs for negative, positve and zero values

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: print + if positive, - if negative and 0 if zero
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
