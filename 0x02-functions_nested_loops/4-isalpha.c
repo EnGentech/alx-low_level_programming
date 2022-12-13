b81aa4542bc4a704d7446f617e9d869ca9c41dec
EnGentech <engen.inyang@gmail.com>
2022-12-13 11:02:55 -0800

    All capital values return 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: _is alpha
+ * Return: always 1 (Success)
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
