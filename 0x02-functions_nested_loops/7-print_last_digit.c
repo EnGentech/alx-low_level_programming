0b734f1cab55a39f12d4ded5a818d501ffd540e7
EnGentech <engen.inyang@gmail.com>
2022-12-13 11:53:48 -0800

    print only last number of the entire digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * Return: Always 0 (Success)
+ * @i: return last value
+ */
+
+int print_last_digit(int i)
+{
+int val;
+val = i % 10;
+_putchar(val);
+}
