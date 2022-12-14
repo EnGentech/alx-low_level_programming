d3b83edc625bc1201a4754a8ac71ff18f52a9f13
EnGentech <engen.inyang@gmail.com>
2022-12-14 03:22:10 -0800

    removed infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
+/*while (i < 10)
 {
-//putchar(i);
+putchar(i);
 }
-
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
