8bdec964ea0f62575eeff122d33c1dac3b66ec2b
EnGentech <engen.inyang@gmail.com>
2022-12-14 01:02:36 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - entry point
- * @0: return the positive or negative result
+ * main - tests function that prints if integer is positive or negative
+ * 
  * Return: always 0 (Success)
  */
 
