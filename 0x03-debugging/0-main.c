6c0bba99ad0f0a4d546d05dab1737e092c07e690
EnGentech <engen.inyang@gmail.com>
2022-12-14 12:22:48 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: always 0 (Success)
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
 int i;
 
