0cf5560b6d059fbe75573aeef523285b74ac68e5
EnGentech <engen.inyang@gmail.com>
2022-12-13 19:32:22 -0800

    iterations of multiple products

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,20 +8,16 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
 _putchar('0');
-
 for (j = 1; j <= n; j++)
 {
 _putchar(',');
 _putchar(' ');
-
 k = i * j;
-
 if (k <= 99)
 _putchar(' ');
 if (k <= 9)
@@ -29,7 +25,7 @@ _putchar(' ');
 if (k >= 100)
 {
 _putchar((k / 100) + '0');
-_putchar(((k / 10)) % 10 + '0');
+ _putchar(((k / 10) % 10) + '0');
 }
 else if (k <= 99 && k >= 10)
 {
