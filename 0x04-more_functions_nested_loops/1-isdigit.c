483b09534b1de69d306cf9fea414805c98085da0
EnGentech <engen.inyang@gmail.com>
2022-12-15 08:42:36 -0800

    return 1 for digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _isdigit: returns 1 for a digit value
- *
- * @c: return 1 for digits only
+ *_isdigit - function that verifies a digit character or not
+ *@c: tested character
+ * Return: returns 1 if it digit
  */
 
 int _isdigit(int c)
 {
 if (c >= 0 && c <= 9)
+{
 return (1);
+}
 else
+{
 return (0);
+}
 }
