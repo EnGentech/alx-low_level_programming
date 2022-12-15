a9685ec0b19bd9bf650588ff2709ce0ff58a9aeb
EnGentech <engen.inyang@gmail.com>
2022-12-15 08:56:23 -0800

    multiply 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: store this value
+ * @b: multiply to a
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
