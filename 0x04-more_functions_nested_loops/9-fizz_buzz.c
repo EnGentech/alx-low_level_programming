5b1cdd9b319ecacdac6aa09b16cfa324ba7abb19
EnGentech <engen.inyang@gmail.com>
2022-12-15 14:35:41 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,7 @@
-
 #include <stdio.h>
 
 /**
- *fizz_buzz
+ * main - fizz_buzz
  *
  *Return: Always 0 (Success)
  */
@@ -15,11 +14,17 @@ for (numb = 1; numb <= 100; numb++)
 if (((numb % 3) == 0) && ((numb % 5) == 0))
 printf("FizzBuzz ");
 else if ((numb % 5) == 0)
+{
+if ((numb != 100))
 printf("Buzz ");
+else
+printf("Buzz");
+}
+    
 else if ((numb % 3) == 0)
 printf("Fizz ");
 else
-printf("%d ",numb);
+printf("%d ", numb);
 }
 putchar('\n');
 return (0);
