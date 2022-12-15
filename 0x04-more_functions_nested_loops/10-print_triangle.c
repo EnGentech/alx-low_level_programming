d1350a3fa077537eacfbc8b9c0ac81b0af413df7
EnGentech <engen.inyang@gmail.com>
2022-12-15 14:45:31 -0800

    print a triangular shape using the #tag

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_triangle - Print a triangular shape in the form of a #tag
+ *@size:size as given by user
+ *Return:returns nothing
+ */
+void print_triangle(int size)
+{
+int db, dd;
+
+if (size > 0)
+{
+for (db = 1; db <= size; db++)
+
+{
+for ((dd = size - dd); dd > 0; dd--)
+_putchar(' ');
+
+for (dd = 0; dd < db; dd++)
+_putchar('#');
+
+if (db == size)
+continue;
+
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
