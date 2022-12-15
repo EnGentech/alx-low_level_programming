8b3d08b70070be0a62efa75155a3fc9b39512513
EnGentech <engen.inyang@gmail.com>
2022-12-15 10:10:19 -0800

    print all numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,11 @@
 void print_most_numbers(void)
 {
 int num = 0;
-while (num <= 9)
+for (num = 0; num <= 9; num++)
 {
-_putchar(num + '0');
-num++;
 if (num == 2 || num == 4)
 continue;
+_putchar(num + '0');
 }
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/try.c
+++ 0x04-more_functions_nested_loops/try.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 int num = 0;
-while (num <= 9)
+ for (num = 0; num <= 9; num++)
 {
-putchar(num + '0');
-num++;
-if (num == 2 || num == 4)
+ if ((num == 2) || (num == 4))
 continue;
+ putchar(num + '0');
+ 
 }
+ putchar('\n');
 }
