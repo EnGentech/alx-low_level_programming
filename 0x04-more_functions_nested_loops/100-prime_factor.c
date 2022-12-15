cef3ee6982fcb05017ac755bba67dab585a2f8e8
EnGentech <engen.inyang@gmail.com>
2022-12-15 14:55:30 -0800

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main- prints the largest prime factor
+ *of a number
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+long n = 612852475143;
+int in;
+while (in++ < n / 2)
+{
+if (n % in == 0)
+{
+n /= 2;
+continue;
+}
+for (in = 3; inc < n / 2; in += 2)
+{
+if (n % in == 0)
+n /= in;
+}
+}
+printf("%ld\n", n);
+return (0);
+}
