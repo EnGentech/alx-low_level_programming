09ae5ff1d2f7ea815ecb504aa84fefe33a211462
EnGentech <engen.inyang@gmail.com>
2022-12-19 03:14:31 -0800

    half values only

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half values from input
+ *
+ * @str: print half of the input
+ */
+
+void puts_half(char *str)
+{
+	int i, count = 0 half = 0;
+
+	for (i = 0; str[i]; i += 1)
+		if (str[i] != '\0')
+			count += 1;
+		else
+			break;
+	half = count / 2;
+	str = *str + half;
+	for (i = 0; i < count; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
