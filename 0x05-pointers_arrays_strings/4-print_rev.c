a59e0c9ab9845d9b1bb0dc5d319487fac173abb5
EnGentech <engen.inyang@gmail.com>
2022-12-19 01:45:11 -0800

    reverse strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse strings
+ *
+ * @s: reverse string
+ */
+
+void print_rev(char *s)
+{
+	int i, count = 0, x;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] != '\0')
+			count += 1;
+		else
+			break;
+	}
+	for (x = count; x >= 0; x--)
+		_putchar(s[x]);
+}
