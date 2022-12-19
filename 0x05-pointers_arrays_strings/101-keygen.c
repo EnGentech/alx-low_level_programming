da04f42f53e63e4e077f3d91092f4b3d6699fa33
EnGentech <engen.inyang@gmail.com>
2022-12-19 05:57:35 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * keygen - generate code to crackme
+ *
+ * Return: generated code
+ */
+
+int main()
+{
+	printf("I need help here");
+}
