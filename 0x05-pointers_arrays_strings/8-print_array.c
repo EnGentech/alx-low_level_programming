7cf417dede7eb71a48d7cd24d36295f149d2f1fd
EnGentech <engen.inyang@gmail.com>
2022-12-19 05:00:26 -0800

    print array of vlues

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if ((i == (n - 1)) || (n == 0))
+		if (i == 0)
+			printf("%d", a[i]);
+		else if (i == (n - 1))
 			printf("%d\n", a[i]);
 		else
 			printf("%d, ", a[i]);
