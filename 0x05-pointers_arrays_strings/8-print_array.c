24077cfb6a4f730e7a025c1dac69cef5d878a81f
EnGentech <engen.inyang@gmail.com>
2022-12-19 04:43:32 -0800

    print array of values

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if ((i == (n - 1)) || (i == n))
+		if ((i == (n - 1)) || (n == 0))
 			printf("%d\n", a[i]);
 		else
 			printf("%d, ", a[i]);
