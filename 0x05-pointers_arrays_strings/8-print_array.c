8998eb8cb8a3888c3919123ce20033de306f8485
EnGentech <engen.inyang@gmail.com>
2022-12-19 03:53:57 -0800

    Print array of values

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - print the content in an array as seen
+ * @n: the array is defined
+ * @a: points to the content of the array n
+ */
+
+void print_array(int *a, int n)
+{
+	for (n = 0; a[n]; n++)
+	{
+		_putchar(a[n]);
+
+	_putchar(',');
+	_putchar(' ');
+	}
+}
