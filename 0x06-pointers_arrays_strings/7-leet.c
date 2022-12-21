0136affd4e0f66788dbe837c346235721cbec133
EnGentech <engen.inyang@gmail.com>
2022-12-21 15:34:31 -0800

    exchange values respectively

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - exchange the values provided with numbers
+ * @ch: Return the exchange values
+ *Return: exchanged values to the pointer
+ */
+char *leet(char *ch)
+{
+	int xcnt, ycnt;
+	char x[] = "aAeEoOtTlL";
+	char y[] = "4433007711";
+
+	stringCount = 0;
+	while (ch[xcnt] != '\0')
+
+	{
+		ycnt = 0;
+		while (ycnt < 10)
+		{
+			if (x[ycnt] == ch[xcnt])
+			{
+				ch[xcnt] = y[ycnt];
+			}
+			ycnt++;
+		}
+		xcnt++;
+	}
+	return (ch);
+}
