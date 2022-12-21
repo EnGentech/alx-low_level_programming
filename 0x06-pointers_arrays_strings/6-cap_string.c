5245bf6366cfb0161a5e13dcf8957802b835533c
EnGentech <engen.inyang@gmail.com>
2022-12-21 14:49:13 -0800

    capitalize each first letter of a word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#incude "main.h"
+
+/**
+ * cap_string - return all first letter of the word to capital letters
+ *
+ * @x: return the values in capital
+ */
+
+char *cap_string(char *x)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		if (x[0] >= 97 && x[0] <= 122)
+		{
+			x[0] = x[0] - 32;
+		}
+		if (x[count] == ' ' || x[count] == '\t' || x[count] == '\n'
+		    || x[count] == ',' || x[count] == ';' || x[count] == '.'
+		        || x[count] == '.' || x[count] == '!' || x[count] == '?'
+		    || x[count] == '"' || x[count] == '(' || x[count] == ')'
+		    || x[count] == '{' || x[count] == '}')
+		{
+			if (x[count + 1] >= 97 && x[count + 1] <= 122)
+			{
+				x[count + 1] = x[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (x);
+}
