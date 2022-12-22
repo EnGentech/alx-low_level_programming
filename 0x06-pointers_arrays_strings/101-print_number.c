52d0ed09c598511fb5ce246fdce006d8b6ee6907
EnGentech <engen.inyang@gmail.com>
2022-12-22 04:51:36 -0800

    putchar the values

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - return the same number as inputted
+ *
+ * @n: return same number as inputted
+ * Return: same number as entered
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
