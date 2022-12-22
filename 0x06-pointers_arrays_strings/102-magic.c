522cc0d93a14a7322627b14f8b9adf25fcfde0f4
EnGentech <engen.inyang@gmail.com>
2022-12-22 05:32:38 -0800

    magic value

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - print the value at array 2
- * Return: Always 0 (success)
- */
-
 int main(void)
 {
 	int n;
@@ -13,9 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-	*(p + 2) = 98;
-
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
