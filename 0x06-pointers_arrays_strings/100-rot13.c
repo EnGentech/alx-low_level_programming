5015fa8b9eb10ebfb28f55fcd895c64b51285bc6
EnGentech <engen.inyang@gmail.com>
2022-12-22 04:12:38 -0800

    rot13 configuration

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rot13 - shift all values in 13 places ahead
+ *
+ * @rot: return the values in 13 place value
+ * Return: return the value as coded
+ */
+
+char *rot13(char *rot)
+{
+	int i;
+
+	for (i = 0; rot[i] >= 97 && rot[i] <= 122; i++)
+	{
+		rot[i] = rot[i] + 13;
+		rot[i]++;
+	}
+	return (rot);
+}
