ded350c142d3d69781337a8d284836d1b21c9260
EnGentech <engen.inyang@gmail.com>
2023-01-02 12:53:29 -0800

    sum of a diagonal matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_diagsums -  print the sum of the diagonal matrix
+ *@a:matrix to be summed
+ *@size:size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
