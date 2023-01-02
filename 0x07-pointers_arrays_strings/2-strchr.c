012814b7494d634968fe7b2079fa2053076865a4
EnGentech <engen.inyang@gmail.com>
2023-01-02 11:30:03 -0800

    strchr user defined

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-	char **ptr = &s;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c && s[i] != '\0')
-			**ptr = *(s + i);
-		else if (s[i] != c)
-			**ptr = '\0';
+			*s = s[i];
+		for (j = s[i]; s[j] != '\0'; j++)
+			return (s);
 	}
-	return (s);
-}
+	return (0);
+		}
