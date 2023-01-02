6f0a1cc56ec87c9a4757b07ea1fb967efd66276e
EnGentech <engen.inyang@gmail.com>
2023-01-02 13:00:03 -0800

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set the pointer to char to
+ *@s: double pointer content
+ *@to: point to variable
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
