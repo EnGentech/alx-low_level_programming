147cfc56050fd452600d899a250830a466b31315
EnGentech <engen.inyang@gmail.com>
2023-01-03 23:42:44 -0800

    count length of strings recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - count the number of strings in s
+ * recursively
+ * @s: whats your length
+ */
+
+int _strlen_recursion(char *s)
+{
+	unsigned int count = 0;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		count += 1;
+	}
+	return (count);
+}
