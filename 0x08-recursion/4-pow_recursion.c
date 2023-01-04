5f80fd2428206d30ec27149b77183eaf94322dbe
EnGentech <engen.inyang@gmail.com>
2023-01-04 00:20:47 -0800

    power of y recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - print the power of y to x
+ * @x: take the base value
+ * @y: take the exponential value to the base of x
+ * Return: return the result generated recursively
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else if (y > 1)
+	{
+		y++;
+		return (x *= x);
+	}
+	return (0);
+}
