fb974a599ad2dc16a18bb40f06132318afc53fa4
EnGentech <engen.inyang@gmail.com>
2023-01-04 00:02:53 -0800

    print factorial recursively

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - print the factorial of n
+ * @n: values provided here should return its factorial equivalent
+ * Return: return factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n >= 1)
+		return (1);
+	else if (n > 1)
+		return (n * factorial(n - 1));
+	return (0);
+}
