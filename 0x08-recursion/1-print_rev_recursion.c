9d63b8e321257db5d8bb29697b7159121195950a
EnGentech <engen.inyang@gmail.com>
2023-01-03 23:27:23 -0800

    reverse strings recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse the strings recursively
+ * @s: take this strings and reverse then printout
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	_putcar(*s);
+	}
+}
