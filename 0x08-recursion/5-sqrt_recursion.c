f5bf06ece3432b421c36243556bba0e8e1a568b8
EnGentech <engen.inyang@gmail.com>
2023-01-04 00:49:57 -0800

    recursive roots of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the square root of n
+ * @n: get the value from user
+ * Return: return the square root of users number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0,n));
+}
+
+/**
+ * _sqrt - return root of a number
+ * @n: check the number
+ * @x: perform squares of a number
+ * Return: return the sqares of users number
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,6 +6,7 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _sqrt(int n, int x);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
