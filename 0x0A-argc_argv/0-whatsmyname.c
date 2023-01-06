6c9ebab758b132adf47083e2a0488cf36ae449c3
EnGentech <engen.inyang@gmail.com>
2023-01-06 07:21:06 -0800

    print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - return my name
+ * @argc: count length
+ * @argv: display my name
+ */
+
+int main(argc, char agrv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Argument count and vectors
