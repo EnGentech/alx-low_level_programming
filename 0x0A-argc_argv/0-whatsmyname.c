d950b98d9420c0983a62ab13638e2a7183c5abc2
EnGentech <engen.inyang@gmail.com>
2023-01-06 07:25:29 -0800

    print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
  * main - return my name
  * @argc: count length
  * @argv: display my name
+ * Return: always 0 Success
  */
 
-int main(argc, char agrv[])
+int main(argc, char argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
