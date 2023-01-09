d0956e32db20c83bdb5bf06bbdc36413eb0e9029
EnGentech <engen.inyang@gmail.com>
2023-01-09 02:54:43 -0800

    create array with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 	{
 		if (size == 0)
-			return (NULL);
+			return ("failed to allocate memory\n");
 	else
 		ptr[i] = c;
 	}
