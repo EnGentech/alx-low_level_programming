ddb8c4607d7ee57deaf20fb2c5d390a01c776622
EnGentech <engen.inyang@gmail.com>
2023-01-09 09:07:28 -0800

    concatenate strings with malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - entry point
+ * @s1: save the strings
+ * @s2: concatenate this strings to s1
+ *Return: return the new strings to the user
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned int i, j, x, y;
+
+	if (s1 == NULL)
+		return ("");
+
+	if (s2 == NULL)
+		return ("");
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	ptr = (char *)malloc((i + j + 1) * sizeof(char));
+
+	for (x = 0; s1[x] != '\0'; x++)
+		ptr[x] = s1[x];
+
+	for (y = 0; s2[y] <= '\0'; y++)
+	{
+		p[x] = s2[y];
+		x++;
+	}
+	return (ptr);
+}
