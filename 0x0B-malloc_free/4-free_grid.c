8795164306a06fd728eb378f6465e029cd8fcd34
EnGentech <engen.inyang@gmail.com>
2023-01-09 10:32:29 -0800

    free malloc memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - entry point
+ * @grid: identify the content
+ * @height: free this location
+ */
+
+void free_grid(int **grid, int height)
+{
+	unsigned int i;
+
+	for(i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
