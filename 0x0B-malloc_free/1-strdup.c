8e63c92d166dd9a74a64a63c8e140cc1c1245b64
EnGentech <engen.inyang@gmail.com>
2023-01-09 04:00:00 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - main entry
+ * @str: point the pointer here
+ * Return: return pointer
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	ptr = (char *)malloc((i + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		ptr[j] = str[i];
+	ptr[i + 1] = '\0';
+	return (ptr);
+}
