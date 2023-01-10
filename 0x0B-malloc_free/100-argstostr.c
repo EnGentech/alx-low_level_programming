5e1120d1b31f2fcb9e290e42d6675edd3744578e
EnGentech <engen.inyang@gmail.com>
2023-01-10 04:07:36 -0800

    concatenate all args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include  "main.h"
+
+/**
+ * argstostr - entry point
+ * @ac: count the number of arguments
+ * @av: take the values in this pointer
+ * Return: return the strings to the standard output
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *arg;
+	int i, j, count = 0;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			count++;
+	}
+	count += ac;
+	arg = (char *)malloc((count + 1) * sizeof(char));
+
+	if (arg == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			arg[n] = av[i][j];
+			n++;
+		}
+		if (arg[n] == '\0')
+		{
+			arg[n++] = '\n';
+		}
+	}
+	return (arg);
+}
