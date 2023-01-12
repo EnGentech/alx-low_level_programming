f4a206001b1715a5fb9c96bfd42f6a7b3d1d6e8f
EnGentech <engen.inyang@gmail.com>
2023-01-12 14:43:32 -0800

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,12 +19,12 @@ int *array_range(int min, int max)
 
 	ptr = (int *)malloc((max - min + 1) * sizeof(int));
 
-	if (ptr = NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i <= (max - min); i++)
 	{
-		prt[i] = min;
+		ptr[i] = min;
 		min++;
 	}
 	return (ptr);
