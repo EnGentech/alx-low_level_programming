68b98e6dbe78330249c6188577f84e129fffc47b
EnGentech <engen.inyang@gmail.com>
2023-01-12 14:21:54 -0800

    allocate memory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - entry point
+ * @nmemb: the array content
+ * @size: size of the memory to be allocated
+ * Return: return the malloc pointer
+ */
+
+void *_calloc(unsinged int nmemb, unsigned int size)
+{
+	unsinged int i, j;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	j = (nmemb * size);
+
+	ptr = malloc(j);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < j; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
