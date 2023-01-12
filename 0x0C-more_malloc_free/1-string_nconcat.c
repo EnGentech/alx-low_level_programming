5d891bb99c2903ba717664c85078d36922d0b24a
EnGentech <engen.inyang@gmail.com>
2023-01-12 12:26:28 -0800

    concatenate strings limited to n value

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - main - entry
+ * @s1: take the characters in this memory
+ * @s2: concatenate this strings to the s1 variable
+ * @n: check for the legnth of the characters in s2
+ * Return: return the pointer to the standard output
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int i;
+	unsigned int j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	ptr = (char *)malloc((n + i) * sizeof(char));
+	if (ptr == NULL)
+		return ("");
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j <= n; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	return (ptr);
+}
