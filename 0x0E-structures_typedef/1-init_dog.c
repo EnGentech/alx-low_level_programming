c945b224c63cbcfb01e4e0de2dd5b33e66464995
EnGentech <engen.inyang@gmail.com>
2023-01-16 09:10:34 -0800

    structure through main.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * init_dog - entry point
+ * @d: point to the memory
+ * @name: take the dogs name
+ * @age: take the dogs age
+ * @owner: take the owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		(d).name = name;
+		(d).age = age;
+		d->owner = owner;
+	}
+}
