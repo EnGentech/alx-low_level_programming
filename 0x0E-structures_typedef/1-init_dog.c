cc467147029be0b9590a2ae7602a62e71be65b45
EnGentech <engen.inyang@gmail.com>
2023-01-16 09:17:50 -0800

    struct through dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 /**
  * init_dog - entry point
@@ -13,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(d).name = name;
-		(d).age = age;
+		(*d).name = name;
+		(*d).age = age;
 		d->owner = owner;
 	}
 }
