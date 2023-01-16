e18113ad156ccb60ebfdd740ff3323e810990ba7
EnGentech <engen.inyang@gmail.com>
2023-01-16 09:24:00 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,9 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif
