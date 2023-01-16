c0d1e24ed845737a60b569eea663362f96eb4c73
EnGentech <engen.inyang@gmail.com>
2023-01-16 07:58:53 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog - entry point for structure
+ * @name: dog's name caption
+ * @age: age of the dog
+ * @owner: who owns the dog
+ */
+
 struct dog
 {
 	char *name;
