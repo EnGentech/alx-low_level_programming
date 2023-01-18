b23968772095b20ecab158ec37dc38ff9fe0e7b3
EnGentech <engen.inyang@gmail.com>
2023-01-18 05:58:10 -0800

    print array content

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - main entry
+ * @array: get the array content
+ * @action: pointer function
+ * @size: take the size of size_t
+ */
+
+void array_iterator(int *array, size_t size, (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[a]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
