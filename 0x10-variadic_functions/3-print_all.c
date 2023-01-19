568433c0800b1e55255603e6d5428f0c0c93aad8
EnGentech <engen.inyang@gmail.com>
2023-01-19 09:57:59 -0800

    print all formats

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - main entry
+ *
+ * @format: take in the format to print
+ */
+
+void print_all(const char * const format, ...)
+{
+	vwitch (format[i])
+	{
+		case 'i':
+			printf("%i", va_arg(vl, int));
+			break;
+			case 'f':
+				printf("%f", va_arg(vl, double));
+				break;
+				case 'c':
+					printf("%c", (char) va_arg(vl, int));
+					break;
+					case 's':
+						string = va_arg(vl, char *);
+						if (string == NULL)
+						{
+							printf("(nil)");
+							break;
+						}
+						printf("%s", string);
+						break;
+	}
+	if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+	     format[i] == 's') && format[(i + 1)] != '\0')
+		printf(", ");
+	i++;
+}
+printf("\n");
+va_end(vl);
+}
