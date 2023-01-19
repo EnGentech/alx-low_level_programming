ebef31d8cc699c1890fd3232f5ba363db7917e7f
EnGentech <engen.inyang@gmail.com>
2023-01-19 09:22:12 -0800

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (s == NULL)
 			printf("(nil)");
-
+		else
 		printf("%s", s);
 
 		if (separator != NULL && i != (n - 1))
