7ccaf3f377a8209ac9e7b907170a53bb06f01e83
EnGentech <engen.inyang@gmail.com>
2023-01-19 06:32:40 -0800

    print numbers with user specific separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - main entry
+ * @separator: take the string to separate the integers
+ * @n: take the number of arguments to be passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list numb;
+
+	va_start(numb, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numb, const unsigned int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	va_end(numb);
+}
