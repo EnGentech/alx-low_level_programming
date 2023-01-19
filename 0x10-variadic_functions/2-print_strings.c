8c2287e9a28dff19b70d789a9d705eb822f79a35
EnGentech <engen.inyang@gmail.com>
2023-01-19 08:07:22 -0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - main entry
+ * @separator: take the character to seperate strings
+ * @n: take the number of arguments to be passed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list albet;
+
+	va_start(albet, n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *s;
+		s = va_arg(albet, char *);
+		printf("%s", s);
+
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+
+		if (s == NULL)
+			printf("(nil)");
+	}
+
+	printf("\n");
+	va_end(albet);
+}
