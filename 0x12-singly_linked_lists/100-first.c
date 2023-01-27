1d427d9f238b2792da02e33915b2bb02cba1079e
EnGentech <engen.inyang@gmail.com>
2023-01-27 10:30:39 -0800

    message before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * before_main - entry point message
+ *
+ * Return:  void the return
+ *
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
