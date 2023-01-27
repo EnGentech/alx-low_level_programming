31849dedac82bb0a30c30832cbc92c08f796f397
EnGentech <engen.inyang@gmail.com>
2023-01-27 10:24:20 -0800

    free malloc memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - entry point to free malloc memory
+ * @head: the head content
+ *
+ *Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next:
+	}
+}
