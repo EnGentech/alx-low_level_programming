6ee5387169286dcd4b3f963d4285d59dfbc1f7bb
EnGentech <engen.inyang@gmail.com>
2023-01-30 13:44:36 -0800

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - list the number of elements in list
+ *
+ * @h: take the values of the node
+ *
+ * Return: return count
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
