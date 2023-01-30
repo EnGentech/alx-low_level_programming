26fa7d5a30ae9dc2e54adb1915a99e37e874a465
EnGentech <engen.inyang@gmail.com>
2023-01-30 15:36:51 -0800

    add nodes to head

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add node to head
+ *
+ *@head: add node
+ * @n: get values to be added
+ *
+ * Return: return added values
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	else
+	{
+		newnode->n = n;
+		newnode->next = *head;
+
+		*head = newnode;
+	}
+	return (newnode);
+}
