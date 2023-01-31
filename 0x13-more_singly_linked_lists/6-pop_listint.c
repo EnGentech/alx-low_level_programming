16f876c61d32a564d87b19fcb8cfeff01d219500
EnGentech <engen.inyang@gmail.com>
2023-01-31 06:24:34 -0800

    pop head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,9 +12,12 @@ int pop_listint(listint_t **head)
 {
 	listint_t *del;
 
+	if ((*head)->next == 0)
+		return (0);
+
 	del = *head;
-	*head = *head->next;
+	*head = (*head)->next;
 	free(del);
 
-	return (head);
+	return ((*head)->n);
 }
