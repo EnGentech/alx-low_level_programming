2ca736e9b5cda7b2287d91233632f284e9c16758
EnGentech <engen.inyang@gmail.com>
2023-01-31 05:41:34 -0800

    check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
-		return NULL;
-
-	while (*head)
+	while (*head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
