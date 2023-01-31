d0ed867a091b3d9144ea95b704af7ed2d4e093cc
EnGentech <engen.inyang@gmail.com>
2023-01-31 14:35:58 -0800

    insert node at certain point

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - insert node at certain index
+ * @head: find the node point
+ * @idx: get the index of the node to be added
+ * @n: get the data to be inserted
+ * Return: return the value added
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
