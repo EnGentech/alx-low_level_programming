a2421e764559ea63c73d206fade84bf815bd1d9c
EnGentech <engen.inyang@gmail.com>
2023-01-31 07:04:52 -0800

    return head index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get the last element of the node
+ *
+ * @index: get the value at the index
+ *
+ * Return: return the value at the index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	for (count = 0; count < index; index++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head =  head->next;
+	}
+	return (head);
+}
