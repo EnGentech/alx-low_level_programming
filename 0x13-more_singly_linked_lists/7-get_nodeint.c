56e60a5ab455680bf27e90e5b2f7f5a603f7a742
EnGentech <engen.inyang@gmail.com>
2023-01-31 07:11:58 -0800

    updated index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
  * get_nodeint_at_index - get the last element of the node
  *
  * @index: get the value at the index
- *
+ * @head: get the node
  * Return: return the value at the index
  */
 
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
 
-	for (count = 0; count < index; index++)
+	for (count = 0; count < index; count++)
 	{
 		if (head == NULL)
 			return (NULL);
