510c99bf6235f7bfad0c30e744166f1c14e4bfa4
EnGentech <engen.inyang@gmail.com>
2023-01-31 05:54:27 -0800

    free head with double pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,12 +14,12 @@ void free_listint2(listint_t **head)
 	if (*head == NULL)
 		return;
 
-	while (*head != NULL)
+	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
