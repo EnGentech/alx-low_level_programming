174390ed59fbd5d11841f6c263b66929b4a5423f
EnGentech <engen.inyang@gmail.com>
2023-02-01 11:11:13 -0800

    modified

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,15 +9,21 @@
 size_t print_listint_safe(const listint_t *head)
 {
        	size_t i = 0;
+	int chk;
 
-	if (head == NULL)
-		exit(98);
-
-	while (head != NULL)
+	while (head)
 	{
+		chk = head - head->next;
 		i++;
-		printf("[%p] %d",(void *)&head, head->n);
-		head = head->next;
-	}
-	return (i);
+
+		printf("[%p] %d\n", (void *)&head, head->n);
+
+		if (chk > 0)
+			head = head->next;
+		else
+		{
+			printf("->[%p] %d\n", (void *)&head, head->next->n);
+			break;
+		}
+		return (i);
 }
