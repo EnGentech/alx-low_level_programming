90a456a30580dd67f8832d15ff436795ce2270a3
EnGentech <engen.inyang@gmail.com>
2023-02-02 09:00:50 -0800

    insert bit at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - set a value to bit length
+ * @index: get the index of bits
+ * Return: return new bits
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+
+	return (1);
+
+}
