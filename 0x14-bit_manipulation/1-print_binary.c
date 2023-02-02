5ffce3cafc1006e8ed7481289a1ba3557217ca40
EnGentech <engen.inyang@gmail.com>
2023-02-02 08:46:03 -0800

    convert to decimal

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary
+ * @n: get the decimal values
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, bit = 0;
+	unsigned long int wise;
+
+	for (i = 63; i >= 0; i--)
+	{
+		wise = n >> i;
+
+		if (wise & 1)
+		{
+			_putchar('1');
+			bit++;
+		}
+		else if (bit)
+			_putchar('0');
+	}
+
+	if (!bit)
+		_putchar('0');
+
+}
