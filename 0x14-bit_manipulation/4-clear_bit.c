82f41612f24a93c7586c1d86f6ad65232267f9ec
EnGentech <engen.inyang@gmail.com>
2023-02-02 09:07:26 -0800

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bits to zero at index\
+ * @n: get bits
+ * @index: set bit to zero
+ * Return: return remaining bits
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+
+}
