1bd1155134cce303cb0a6002cabab1216b8dd4ea
EnGentech <engen.inyang@gmail.com>
2023-02-02 06:18:38 -0800

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - convert values to decimal
+ * @b: get the input from user
+ *
+ * Return: return the converted value to standard output
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned i, dec = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+
+		dec = 2 * dec + (b[i] - '0');
+	}
+
+	return (dec);
+}
