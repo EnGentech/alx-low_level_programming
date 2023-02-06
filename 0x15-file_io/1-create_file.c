0fb2e2393ad6b860c35b9df7e3618df0dca3a37d
EnGentech <engen.inyang@gmail.com>
2023-02-06 14:27:28 -0800

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - create file using file handler
+ * @filename: get the file name
+ * @text_content: get the text contents
+ * Return: return the file created
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
