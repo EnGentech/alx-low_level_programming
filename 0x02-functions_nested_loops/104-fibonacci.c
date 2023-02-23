cf56696d4e27698979c07a2f551ddf7699baa66d
EnGentech <engen.inyang@gmail.com>
2023-02-23 03:27:23 -0800

    104 fibon added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 1, j = 2, fib;
+	unsigned long sum = i + j;
+	unsigned long fi = sum + j;
+	unsigned long new_fib = 0;
+
+	printf("1, 2, 3, 5, ");
+
+	for (fib = 5; fib <= 98; fib++)
+	{
+		new_fib = fi + sum;
+		sum = fi;
+		fi = new_fib;
+
+		if (fib == 50)
+		{
+			printf("%lu", new_fib);
+		}
+		else
+		{
+			printf("%lu, ", new_fib);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
