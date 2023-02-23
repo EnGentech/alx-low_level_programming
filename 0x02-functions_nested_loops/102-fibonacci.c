3a73409cdfe9b2e868fd7112bd2011308201aed8
EnGentech <engen.inyang@gmail.com>
2023-02-23 03:02:46 -0800

    fibonacci 102 updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,44 +7,28 @@
 
 int main(void)
 {
-int count;
-unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-unsigned long half1, half2;
+	int i = 1, j = 2, fib;
+	unsigned long sum = i + j;
+	unsigned long fi = sum + j;
+	unsigned long new_fib = 0;
 
-for (count = 0; count < 92; count++)
-{
-sum = fib1 + fib2;
-printf("%lu, ", sum);
+	printf("1, 2, 3, 4, 5, ");
 
-fib1 = fib2;
-fib2 = sum;
-}
+	for (fib = 5; fib <= 50; fib++)
+	{
+		new_fib = fi + sum;
+		sum = fi;
+		fi = new_fib;
 
-fib1_half1 = fib1 / 10000000000;
-fib2_half1 = fib2 / 10000000000;
-fib1_half2 = fib1 % 10000000000;
-fib2_half2 = fib2 % 10000000000;
-
-for (count = 93; count < 99; count++)
-{
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half2 > 9999999999)
-{
-half1 += 1;
-half2 %= 10000000000;
-}
-
-printf("%lu%lu", half1, half2);
-if (count != 98)
-printf(", ");
-
-fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half2;
-fib2_half1 = half1;
-fib2_half2 = half2;
-}
-printf("\n");
-return (0);
+		if (fib == 50)
+		{
+			printf("%lu", new_fib);
+		}
+		else
+		{
+			printf("%lu, ", new_fib);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
