fca8e3c97cc477262fc059bd383b82c1d639e062
EnGentech <engen.inyang@gmail.com>
2023-03-02 13:02:52 -0800

    edited rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,10 +13,10 @@ char *rot13(char *rot)
 
 	for (i = 0; rot[i] != '\0'; i++)
 	{
-		if (rot[i] >= 65 && rot[i] <= 77 || rot[i] >= 97 && rot[i] <= 109)
+		if ((rot[i] >= 65 && rot[i] <= 77) || (rot[i] >= 97 && rot[i] <= 109))
 			rot[i] = rot[i] - 13;
-		else if (rot[i] >= 77 && rot[i] <= 90 || rot[i] > 109 && rot[i] <= 122)
+		else if ((rot[i] >= 77 && rot[i] <= 90) || (rot[i] > 109 && rot[i] <= 122))
 			rot[i] = rot[i] + 13;
 	}
-	return (rot);
+	return (0);
 }
