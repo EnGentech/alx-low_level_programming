64cce89f6586d01b85321b7e0567ef46a4abe14e
EnGentech <engen.inyang@gmail.com>
2023-07-12 21:56:30 -0700

    added header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,10 @@
 #define ALGOS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
